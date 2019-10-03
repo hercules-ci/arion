5f56a0846a55df63ea41f5b201627da4b8cc9327
Robert Hensing <robert@roberthensing.nl>
2019-10-03 17:54:48 +0200

    Make service.{host, composition} readOnly

--- src/nix/modules/service/context.nix
+++ src/nix/modules/service/context.nix
@@ -3,12 +3,14 @@
   options = {
     host = lib.mkOption {
       type = lib.types.attrs;
+      readOnly = true;
       description = ''
         The composition-level host option values.
       '';
     };
     composition = lib.mkOption {
       type = lib.types.attrs;
+      readOnly = true;
       description = ''
         The composition configuration.
       '';
