4b6302a613ab5450d557113403dc47125cf1df28
Robert Hensing <robert@roberthensing.nl>
2019-10-07 16:22:29 +0200

    Extract the webservice service into file

--- examples/minimal/arion-compose.nix
+++ examples/minimal/arion-compose.nix
@@ -2,16 +2,11 @@
 {
   config.docker-compose.services = {
 
-    webserver = {
-      service.useHostStore = true;
-      service.command = [ "sh" "-c" ''
-                  cd "$$WEB_ROOT"
-                  ${pkgs.python3}/bin/python -m http.server
-                '' ];
-      service.ports = [
-        "8000:8000" # host:container
-      ];
-      service.environment.WEB_ROOT = "${pkgs.nix.doc}/share/doc/nix/manual";
+    # webserver.imports = [ ./service-webserver.nix ];
+
+    webserver = {...}: {
+      imports = [ ./service-webserver.nix ];
     };
+
   };
 }
