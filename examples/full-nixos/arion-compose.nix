e73710caf92dbc9c9f8f6f4e153adbc377a84138
Robert Hensing <robert@roberthensing.nl>
2021-05-31 15:15:03 +0200

    examples/full-nixos: Disable nscd

--- examples/full-nixos/arion-compose.nix
+++ examples/full-nixos/arion-compose.nix
@@ -4,6 +4,8 @@
     nixos.configuration.boot.tmpOnTmpfs = true;
     nixos.configuration.services.nginx.enable = true;
     nixos.configuration.services.nginx.virtualHosts.localhost.root = "${pkgs.nix.doc}/share/doc/nix/manual";
+    nixos.configuration.services.nscd.enable = false;
+    nixos.configuration.system.nssModules = lib.mkForce [];
     nixos.configuration.systemd.services.nginx.serviceConfig.AmbientCapabilities = 
       lib.mkForce [ "CAP_NET_BIND_SERVICE" ];
     service.useHostStore = true;
