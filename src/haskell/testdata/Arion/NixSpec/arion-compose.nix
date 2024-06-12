8f0549b434381a85671e4f2c1606979cd703b6d0
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-06-12 22:14:14 +0000

    rename boot.tmpOnTmpfs -> boot.tmp.useTmpfs
    resolves warning:
    trace: Obsolete option `boot.tmpOnTmpfs' is used. It was renamed to
    `boot.tmp.useTmpfs'.
    c.f.
    https://github.com/NixOS/nixpkgs/commit/a5d95ac5fca37482f55feb0870c9ed6b1b9bb4b4

--- src/haskell/testdata/Arion/NixSpec/arion-compose.nix
+++ src/haskell/testdata/Arion/NixSpec/arion-compose.nix
@@ -2,7 +2,7 @@
   project.name = "unit-test-data";
   services.webserver = { pkgs, ... }: {
     nixos.useSystemd = true;
-    nixos.configuration.boot.tmpOnTmpfs = true;
+    nixos.configuration.boot.tmp.useTmpfs = true;
     nixos.configuration.services.nginx.enable = true;
     nixos.configuration.services.nginx.virtualHosts.localhost.root = "${pkgs.nix.doc}/share/doc/nix/manual";
     service.useHostStore = true;
