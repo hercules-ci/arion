7d0e26647c7219408fdf13bb5fc59faa8a956c5d
Kiara Grouwstra <kiara.grouwstra@gmail.com>
2024-08-03 18:31:46 +0000

    add docker executable, needed for swarm

--- nix/upstreamable/default.nix
+++ nix/upstreamable/default.nix
@@ -57,7 +57,7 @@ let
       mv $out/bin/arion $out/libexec
       makeWrapper $out/libexec/arion $out/bin/arion \
         --unset PYTHONPATH \
-        --prefix PATH : ${lib.makeBinPath [ pkgs.docker-compose ]} \
+        --prefix PATH : ${lib.makeBinPath [ pkgs.docker-compose pkgs.docker ]} \
         ;
     '';
   };
