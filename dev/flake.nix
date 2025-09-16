6036309c3666e65d3f3aeb6f06968223c1d50b99
Robert Hensing <robert@roberthensing.nl>
2025-09-16 17:57:15 +0200

    dev/flake: Update hercules-ci-effects
    Flake lock file updates:
    
    • Updated input 'hercules-ci-effects':
        'github:hercules-ci/hercules-ci-effects/1a3667d33e247ad35ca250698d63f49a5453d824?narHash=sha256-ENUhCRWgSX4ni751HieNuQoq06dJvApV/Nm89kh%2B/A0%3D' (2025-09-16)
      → 'github:hercules-ci/hercules-ci-effects/f1419cf77e518c16c8d57924f94f8024779be1f9?narHash=sha256-3CLsQr9HWm2xuxtqsFrPfocFQELC%2B80Ho7FGkJ0r9GU%3D' (2025-09-16)

--- dev/flake.nix
+++ dev/flake.nix
@@ -2,7 +2,7 @@
   # TODO move back into main lock after https://github.com/NixOS/nix/issues/7730, remove this subflake
   description = "Development dependencies in a separate subflake so that they don't end up in your lock";
   inputs = {
-    hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
+    hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects/flake-update-merge-ff-and-enable";
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
   };
   outputs = { ... }: { };
